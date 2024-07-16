92cff217edb4d218e12d94f008e37943dff50fa5
KangDongJoon <107305960+KangDongJoon@users.noreply.github.com>
2024-07-16 20:10:09 +0900

    Update 정수 제곱근 판별.java
    불필요 형변환 및 변수선언 제거

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,13 +1,11 @@
 class Solution {
     public long solution(long n) {
+        long sqrt = (long) Math.sqrt(n);
         
-        double dn = n;
-        double sqrt = Math.sqrt(dn);
-        long lsqrt = (long)sqrt;
-        double dsqrt = lsqrt;
-        if(sqrt == dsqrt){
-            return (long)Math.pow((sqrt+1), 2);
-        }else{
+        // 제곱근의 제곱이 원래 숫자와 같다면, 정수 제곱근이다.
+        if (sqrt * sqrt == n) {
+            return (long)Math.pow((sqrt + 1), 2);
+        } else {
             return -1;
         }
     }
