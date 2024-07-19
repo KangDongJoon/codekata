4199e4557948afc0945e3cb9ebdc8390104250db
KangDongJoon <107305960+KangDongJoon@users.noreply.github.com>
2024-07-19 18:29:24 +0900

    [level 1] Title: 최소직사각형, Time: 1.83 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,6 +1,31 @@
 class Solution {
     public int solution(int[][] sizes) {
-        int answer = 0;
+        int arm = Integer.MIN_VALUE;
+        int acm = Integer.MIN_VALUE;
+        spin(sizes);
+        
+        for(int i = 0; i < sizes.length; i++){
+            if(sizes[i][0] > arm){
+                arm = sizes[i][0];
+            }if(sizes[i][1] > acm){
+                acm = sizes[i][1];
+            }
+        }
+        int answer = arm * acm;
+        
         return answer;
+
+    }
+
+    public static int[][] spin(int[][] arr) {
+        int tmp = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i][0] > arr[i][1]){
+                tmp = arr[i][0];
+                arr[i][0] = arr[i][1];
+                arr[i][1] = tmp;
+            }
+        }
+        return arr;
     }
 }
