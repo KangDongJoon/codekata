983150108a64b2593257f68f242ce6e9698359c6
KangDongJoon <107305960+KangDongJoon@users.noreply.github.com>
2024-07-22 10:58:20 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 5.20 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -7,12 +7,11 @@ class Solution {
 		for(int i = 0; i < s.length(); i++) {
 			String c  = Character.toString(s.charAt(i));
 			if(!map.containsKey(c)) {
-				map.put(c, i);
 				answer[i] = -1;
 			}else {
 				answer[i] = i - map.get(c);
-				map.put(c, i);
 			}
+            map.put(c, i);
 		}
         return answer;
     }
