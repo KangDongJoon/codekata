ea2df7d73978cf93625165b97f640bb515bab4fa
KangDongJoon <107305960+KangDongJoon@users.noreply.github.com>
2024-07-23 13:16:05 +0900

    [level 1] Title: 2016년, Time: 0.03 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -1,37 +1,14 @@
-import java.util.*;
 class Solution {
     public String solution(int a, int b) {
-        String answer = week(day(a,b));
+        int[] year = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] week = {"THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"};
+        int day = 0;
+        for(int i = 0; i < a - 1; i++){
+            day += year[i];
+        }
+        day += b;
+        
+        String answer = week[(day % 7)] ;
         return answer;
     }
-    public static int day(int a, int b) {
-		int day = 0;
-		if(a == 1) {
-			return b;
-		}else {
-			for(int i = 1; i < a; i++) {
-				if(i == 1 || i == 3 || i == 5 || i == 7 || i == 8 || i == 10 || i == 12) {
-					day += 31;
-				}else if(i == 4 || i == 6 || i == 9 || i == 11) {
-					day += 30; 
-				}else {
-					day += 29;
-				}
-			}
-			day += b;
-		}return day;
-	}
-	
-	public static String week(int d) {
-		Map<Integer, String> wek = new HashMap<>();
-		wek.put(1, "FRI");
-		wek.put(2, "SAT");
-		wek.put(3, "SUN");
-		wek.put(4, "MON");
-		wek.put(5, "TUE");
-		wek.put(6, "WED");
-		wek.put(0, "THU");
-		
-		return wek.get(d % 7);
-	}
 }
