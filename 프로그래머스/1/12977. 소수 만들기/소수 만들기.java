71cd35e51523caf213e11b38820f20baba1594fc
KangDongJoon <107305960+KangDongJoon@users.noreply.github.com>
2024-07-25 10:49:10 +0900

    [level 1] Title: 소수 만들기, Time: 7.59 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -1,33 +1,24 @@
-import java.util.*;
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
+        int sum = 0;
 
-        // 배열 정렬
-        Arrays.sort(nums);
-
-        // 첫 번째 숫자를 고정하고 두 번째와 세 번째 숫자를 포인터로 탐색
         for (int i = 0; i < nums.length - 2; i++) {
             int l = i + 1;
             int r = nums.length - 1;
 
             while (l < r) {
-                int sum = nums[i] + nums[l] + nums[r];
-
-                if (prime(sum) == 1) {
-                    answer++;
-                }
-
-                // l과 r 포인터를 이동하여 모든 조합을 탐색
-                l++;
-                if (l >= r) {
-                    r--;
-                    l = i + 1;
+                    sum = nums[i] + nums[l] + nums[r];
+                    answer += prime(sum);
+                    l++;
+                    if(l >= r){
+                        r--;
+                        l = i + 1;
+                    }
                 }
             }
-        }
         return answer;
-    }
+        }
 
 
     public static int prime(int sum) {
