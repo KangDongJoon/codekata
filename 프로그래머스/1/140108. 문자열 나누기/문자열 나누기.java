dc17e7aae48668f06d70456c1125478c6a759d1d
KangDongJoon <107305960+KangDongJoon@users.noreply.github.com>
2024-08-05 22:02:45 +0900

    [level 1] Title: 문자열 나누기, Time: 1.11 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -1,6 +1,38 @@
 class Solution {
-    public int solution(String s) {
+    public int solution(String S) {
         int answer = 0;
+
+        int i = 0;
+        outLoop:
+        while(i < S.length()) {
+            if(i == S.length() - 1){
+                answer++;
+                break;
+            }
+            char x = S.charAt(i);
+            int xCnt = 1;
+            int yCnt = 0;
+            for(int j = i + 1; j < S.length(); j++){
+                char y = S.charAt(j);
+                if(x == y){
+                    xCnt++;
+                }else{
+                    yCnt++;
+                }
+                if(xCnt == yCnt) {
+                    answer++;
+                    if (j < S.length() - 1) {
+                        i = j + 1;
+                        break;
+                    }else{
+                        break outLoop;
+                    }
+                }else if(xCnt != yCnt && j == S.length() - 1){
+                    answer++;
+                    break outLoop;
+                }
+            }
+        }
         return answer;
     }
 }
