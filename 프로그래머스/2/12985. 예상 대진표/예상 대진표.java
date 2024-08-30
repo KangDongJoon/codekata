b91d8db392f3b604b960972cac2e3a3d4d77f19a
KangDongJoon <107305960+KangDongJoon@users.noreply.github.com>
2024-08-30 09:38:37 +0900

    [level 2] Title: 예상 대진표, Time: 0.12 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -2,19 +2,20 @@ class Solution{
     public int solution(int n, int a, int b){
         int s = 1;
         int answer = 0; 
-        if(a % 2 != 0){
-            if(a + 1 == b){
-                return 1;
-            }
-        }else{
-            if(a - 1 == b){
-                return 1;
-            }
-        }
-
 
         for(int i = 1; s != n; i++){
             s = (int)Math.pow(2, i);
+            if(a % 2 != 0){
+                if(a + 1 == b){
+                    answer = i;
+                    break;
+                }
+            }else{
+                if(a - 1 == b){
+                    answer = i;
+                    break;
+                }
+            }
             if(a % 2 != 0){
                 a++;
                 a /= 2;
@@ -29,17 +30,6 @@ class Solution{
                 b /= 2;
             }
 
-            if(a % 2 != 0){
-                if(a + 1 == b){
-                    answer = i + 1;
-                    break;
-                }
-            }else{
-                if(a - 1 == b){
-                    answer = i + 1;
-                    break;
-                }
-            }
         }
         return answer;
     }
