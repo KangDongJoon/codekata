f7dbd63742ea51126c38193600ed5cf10d29d112
KangDongJoon <107305960+KangDongJoon@users.noreply.github.com>
2024-11-27 19:19:52 +0900

    [Bronze V] Title: 등록, Time: 92 ms, Memory: 14056 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.time.LocalDate;
+
+public class Main {
+//    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println(87);
+        System.out.println("ehdwns1118");
+
+
+    }
+}
