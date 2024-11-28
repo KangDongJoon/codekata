c57b507d3d0c804a93fb2bff693168cc0d7d93ca
KangDongJoon <107305960+KangDongJoon@users.noreply.github.com>
2024-11-28 19:05:07 +0900

    [Bronze V] Title: N 찍기, Time: 184 ms, Memory: 17956 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.java
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -2,16 +2,19 @@ import java.io.*;
 
 public class Main {
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
 
         int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
         for (int i = 1; i <= n; i++){
-            System.out.println(i);
+            sb.append(i).append("\n");
         }
 
-        bw.flush();
+        System.out.println(sb);
+
+//        bw.flush();
     }
 }
