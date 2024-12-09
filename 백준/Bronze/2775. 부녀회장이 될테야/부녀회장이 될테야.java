b5f9b6cd9f9605e39cc4caf49c5ed0b33e025b14
KangDongJoon <107305960+KangDongJoon@users.noreply.github.com>
2024-12-10 02:30:36 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 92 ms, Memory: 14064 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -1,27 +1,33 @@
 import java.io.*;
-import java.util.*;
+
 public class Main {
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        int[][] arr = new int[15][15];
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= 14; i++) {
-            arr[0][i] = i;  //0층 i호에는 무조건 i명이 산다
-        }
 
-        
-        for (int i = 1; i <= 14; i++) {
-            for (int j = 1; j <= 14; j++) {
-                arr[i][j] = arr[i][j-1] + arr[i-1][j];
-            }
-        }
-        for (int i = 0; i < t; i++) {
-            int k = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
             int n = Integer.parseInt(br.readLine());
-            sb.append(arr[k][n] + "\n");  //k층 n호에 있는 사람 수 출력
-        } 
-        System.out.print(sb);
+            int k = Integer.parseInt(br.readLine());
+            int[][] arr = new int[n + 1][14];
+
+            for (int j = 1; j < 15; j++) {
+                arr[0][j - 1] = j;
+            }
+
+            for (int a = 1; a <= n; a++) {
+                int sum = 0;
+                for (int b = 0; b < 14; b++) {
+                    sum += arr[a - 1][b];
+                    arr[a][b] = sum;
+                }
+            }
+            int answer = arr[n][k - 1];
+            bw.write(Integer.toString(answer));
+            bw.newLine();
+        }
+        bw.flush();
     }
 }
