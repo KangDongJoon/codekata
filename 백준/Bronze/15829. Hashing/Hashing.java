15ce8d3696a7060a19cebacebffb8aff26b359ff
KangDongJoon <107305960+KangDongJoon@users.noreply.github.com>
2024-12-12 19:33:18 +0900

    [Bronze II] Title: Hashing, Time: 108 ms, Memory: 14480 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.math.BigInteger;
 
 public class Main {
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -9,12 +10,16 @@ public class Main {
         int L = Integer.parseInt(br.readLine());
         String read = br.readLine();
 
-        int sum = 0;
+        BigInteger sum = BigInteger.valueOf(0);
         for(int i = 0; i < L; i++){
-            sum += (int) ((read.charAt(i) - 96) * Math.pow(31, i));
+            int c = read.charAt(i) - 96;
+            BigInteger num = BigInteger.valueOf(c).multiply(BigInteger.valueOf(31).pow(i));
+            sum = sum.add(num);
         }
+        sum = sum.mod(BigInteger.valueOf(1234567891));
 
-        bw.write(Integer.toString(sum));
+        bw.write(sum.toString());
         bw.flush();
+
     }
 }
