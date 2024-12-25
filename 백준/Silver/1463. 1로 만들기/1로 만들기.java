af57d5657f0d80b3d11e307d7b40bdaf049e2a96
KangDongJoon <107305960+KangDongJoon@users.noreply.github.com>
2024-12-26 00:38:18 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 18156 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,32 +1,30 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int X = sc.nextInt();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        // DP 배열 선언
-        int[] dp = new int[X + 1];
-        
-        // DP 초기화
-        dp[1] = 0; // 1은 연산 필요 없음
-        
-        for (int i = 2; i <= X; i++) {
-            // 1을 빼는 경우
+    public static void main(String[] args) throws IOException {
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n + 1];
+        dp[1] = 0;
+
+        for(int i = 2; i <= n; i++){
+
             dp[i] = dp[i - 1] + 1;
 
-            // 2로 나누어 떨어질 경우
-            if (i % 2 == 0) {
+            if(i % 2 == 0){
                 dp[i] = Math.min(dp[i], dp[i / 2] + 1);
             }
 
-            // 3으로 나누어 떨어질 경우
-            if (i % 3 == 0) {
+            if(i % 3 == 0){
                 dp[i] = Math.min(dp[i], dp[i / 3] + 1);
             }
         }
 
-        // 결과 출력
-        System.out.println(dp[X]);
+        System.out.println(dp[n]);
+
     }
 }
