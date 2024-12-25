6808104bffd98fcccf24054c6d9bcec6bfe6d8d5
KangDongJoon <107305960+KangDongJoon@users.noreply.github.com>
2024-12-26 00:11:51 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 124 ms, Memory: 15328 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -1,61 +1,65 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-	
-	static int N, M;
-	static int[][] rel;
-	static final int INF = 999999999;
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		rel = new int[N+1][N+1]; //1~N번이라 +1
-		
-		for(int i=1;i<=N;i++) {
-			for(int j=1;j<=N;j++) {
-				rel[i][j] = INF;
-				if(i == j) rel[i][j] = 0;
-			}
-		}
-		
-		for(int i=0;i<M;i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			
-			rel[a][b] = 1; //양방향
-			rel[b][a] = 1;
-		}
-		
-		for(int k=1;k<=N;k++) { //중간지점
-			for(int i=1;i<=N;i++) {
-				for(int j=1;j<=N;j++) {
-					rel[i][j] = Math.min(rel[i][j], rel[i][k]+rel[k][j]);
-				}
-			}
-		}
-		int temp = INF;
-		int result = 0;
-		for(int i=1;i<=N;i++) {
-			int total = 0;
-			for(int j=1;j<=N;j++) {
-				total += rel[i][j];
-			}
-			
-			if(temp > total) { //커야하니까 작은 index 처리도 가능
-				temp = total;
-				result = i;
-			}	
-		}
-		
-		bw.write(result+"");
-		bw.flush();
-		bw.close();
-	}
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N, M;
+    static int[][] relation; // 인접 행렬
 
+    public static void main(String[] args) throws IOException {
+        // 입력 처리
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        relation = new int[N + 1][N + 1];
+
+        // 친구 관계 입력
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            relation[a][b] = relation[b][a] = 1; // 양방향 연결
+        }
+
+        // 최소 베이컨 수 계산
+        int answer = 0;
+        int minBacon = Integer.MAX_VALUE;
+        for (int i = 1; i <= N; i++) {
+            int bacon = bfs(i); // BFS로 각 노드의 베이컨 수 계산
+            if (bacon < minBacon) {
+                minBacon = bacon;
+                answer = i;
+            } else if (bacon == minBacon && i < answer) {
+                answer = i;
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+    // BFS로 특정 노드의 베이컨 수 계산
+    static int bfs(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[N + 1];
+        int[] distance = new int[N + 1];
+
+        queue.add(start);
+        visited[start] = true;
+
+        int bacon = 0;
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+
+            for (int i = 1; i <= N; i++) {
+                if (relation[current][i] == 1 && !visited[i]) {
+                    visited[i] = true;
+                    queue.add(i);
+                    distance[i] = distance[current] + 1;
+                    bacon += distance[i]; // 베이컨 수 누적
+                }
+            }
+        }
+
+        return bacon;
+    }
 }
