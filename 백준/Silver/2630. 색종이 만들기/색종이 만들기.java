d9861a11751e47d8ff9d77d2465a88ea687cc1c8
KangDongJoon <107305960+KangDongJoon@users.noreply.github.com>
2025-01-03 00:23:58 +0900

    [Silver II] Title: 색종이 만들기, Time: 148 ms, Memory: 15664 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -1,70 +1,77 @@
-import java.util.Scanner;
- 
- 
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class Main {
-	
-	// 색상 카운트 할 변수 및 색종이(board)
-	public static int white = 0;
-	public static int blue = 0;
-	public static int[][] board;
- 
-	public static void main(String[] args) {
-		
-		Scanner in = new Scanner(System.in);
-		
-		int N = in.nextInt();
-		
-		board = new int[N][N];
-		
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < N; j++) {
-				board[i][j] = in.nextInt();
-			}
-		}
-		
-		partition(0, 0, N);
-		
-		System.out.println(white);
-		System.out.println(blue);
-		
-	}
-	
-	public static void partition(int row, int col, int size) {
-		
-		//
-		if(colorCheck(row, col, size)) {
-			if(board[row][col] == 0) {
-				white++;
-			}
-			else {
-				blue++;
-			}
-			return;
-		}
-		
-		int newSize = size / 2;	// 절반 사이즈
-		// 재귀 호출
-		partition(row, col, newSize);						// 2사분면
-		partition(row, col + newSize, newSize);				// 1사분면
-		partition(row + newSize, col, newSize);				// 3사분면
-		partition(row + newSize, col + newSize, newSize);	// 4사분면
-	}
-	
-	
-	// 현재 파티션의 컬러가 같은지 체크한다.
-	public static boolean colorCheck(int row, int col, int size) {
-	
-		int color = board[row][col];	// 첫 번째 원소를 기준으로 검사
-		
-		for(int i = row; i < row + size; i++) {
-			for(int j = col; j < col + size; j++) {
-				// 색상이 같이 않다면 false를 리턴 
-				if(board[i][j] != color) {
-					return false;
-				}
-			}
-		}
-		// 검사가 모두 통과했다는 의미이므로 true 리턴
-		return true;
-	}
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    static int N;
+    static int[][] paper;
+    static int white;
+    static int blue;
+
+    public static void main(String[] args) throws IOException {
+
+        // 입력
+        N = Integer.parseInt(br.readLine());
+        paper = new int[N][N];
+        white = 0;
+        blue = 0;
+
+        // 색종이 생성
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                paper[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        search(0, 0, N);
+
+        System.out.println(white);
+        System.out.println(blue);
+    }
+
+    static void search(int row, int col, int size) {
+
+        if (size < 1) {
+            return;
+        }
+
+        if (check(row, col, size)) {
+            if (paper[col][row] == 1) {
+                blue++;
+            } else {
+                white++;
+            }
+            return;
+        }
+
+        // 재귀 호출
+        int newSize = size / 2;
+        // 1사분면
+        search(row + newSize, col, newSize);
+        // 2사분면
+        search(row, col, newSize);
+        // 3사분면
+        search(row, col + newSize, newSize);
+        // 4사분면
+        search(row + newSize, col + newSize, newSize);
+
+    }
+
+    static boolean check(int row, int col, int size) {
+
+        int color = paper[col][row];
+
+        for (int i = col; i < col + size; i++) {
+            for (int j = row; j < row + size; j++) {
+                if (paper[i][j] != color) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
 }
