5d2e6fb591e0afe048521ddf99e9ca4178d135c3
KangDongJoon <107305960+KangDongJoon@users.noreply.github.com>
2025-01-07 18:08:27 +0900

    [Silver V] Title: 집합, Time: 1148 ms, Memory: 300260 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -4,9 +4,8 @@ public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    static int[] arr = new int[21];
     public static void main(String[] args) throws IOException {
-
+        int bitMask = 0;
         int n = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < n; i++) {
@@ -14,67 +13,29 @@ public class Main {
 
             switch (command[0]) {
                 case "add":
-                    add(Integer.parseInt(command[1]));
+                    bitMask |= (1 << (Integer.parseInt(command[1]) - 1));
                     break;
-                case  "remove":
-                    remove(Integer.parseInt(command[1]));
+                case "remove":
+                    bitMask &= ~(1 << (Integer.parseInt(command[1]) - 1));
                     break;
                 case "check":
-                    bw.write(Integer.toString(check(Integer.parseInt(command[1]))));
-                    bw.newLine();
+                    if ((bitMask & (1 << (Integer.parseInt(command[1]) - 1))) != 0) {
+                        bw.write("1\n");
+                    } else {
+                        bw.write("0\n");
+                    }
                     break;
                 case "toggle":
-                    toggle(Integer.parseInt(command[1]));
+                    bitMask ^= (1 << (Integer.parseInt(command[1]) - 1));
                     break;
                 case "all":
-                    all();
+                    bitMask = (1 << 21) - 1;
                     break;
                 case "empty":
-                    empty();
+                    bitMask = 0;
                     break;
             }
-
         }
         bw.flush();
     }
-
-    static void add(int x) {
-        if(arr[x] == x){
-            return;
-        }
-        arr[x] = x;
-    }
-
-    static void remove(int x) {
-        if(arr[x] == 0){
-            return;
-        }
-        arr[x] = 0;
-    }
-
-    static int check(int x) {
-        if(arr[x] == x){
-            return 1;
-        }else{
-            return 0;
-        }
-    }
-
-    static void toggle(int x) {
-        if(arr[x] == x){
-            arr[x] = 0;
-        }else{
-            arr[x] = x;
-        }
-    }
-    
-    static void all() {
-        arr = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
-    }
-
-    static void empty() {
-        arr = new int[21];
-    }
-
 }
-
