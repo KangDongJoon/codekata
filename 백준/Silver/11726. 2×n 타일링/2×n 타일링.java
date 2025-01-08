9487eb9a8f2b5d3116a67f47ebb85d5400dfcbc5
KangDongJoon <107305960+KangDongJoon@users.noreply.github.com>
2025-01-08 22:43:57 +0900

    [Silver III] Title: 2×n 타일링, Time: 104 ms, Memory: 14368 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.math.BigInteger;
 
 public class Main {
 
@@ -11,16 +10,14 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
 
         int size = Math.max(n + 1, 3);
-        BigInteger[] arr = new BigInteger[size];
-        arr[1] = BigInteger.valueOf(1);
-        arr[2] = BigInteger.valueOf(2);
+        int[] arr = new int[size];
+        arr[1] = 1;
+        arr[2] = 2;
 
         for(int i = 3; i < n + 1; i++){
-            arr[i] = arr[i - 1].add(arr[i - 2]);
+            arr[i] = (arr[i - 1] + arr[i - 2]) % 10007;
         }
 
-        int answer = arr[n].mod(BigInteger.valueOf(10007)).intValue();
-
-        System.out.println(answer);
+        System.out.println(arr[n]);
     }
 }
