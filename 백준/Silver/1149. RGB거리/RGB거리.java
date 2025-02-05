86c6350020a1a45148fb1aeb8f79431bfc0dbc3d
KangDongJoon <107305960+KangDongJoon@users.noreply.github.com>
2025-02-05 12:50:09 +0900

    [Silver I] Title: RGB거리, Time: 120 ms, Memory: 14600 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+
+
+    public static void main(String[] args) throws IOException {
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[N][3];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 3; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int i = 1; i < N; i++){
+            arr[i][0] += Math.min(arr[i - 1][1], arr[i - 1][2]);
+            arr[i][1] += Math.min(arr[i - 1][0], arr[i - 1][2]);
+            arr[i][2] += Math.min(arr[i - 1][0], arr[i - 1][1]);
+        }
+
+        int answer = Math.min(arr[N - 1][0], Math.min(arr[N- 1][1], arr[N - 1][2]));
+
+        System.out.println(answer);
+
+    }
+}
