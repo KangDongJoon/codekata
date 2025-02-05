e957de8fe4d4bb69a62b626e366fe9e6688ff7d2
KangDongJoon <107305960+KangDongJoon@users.noreply.github.com>
2025-02-05 12:53:59 +0900

    [Silver I] Title: RGB거리, Time: 112 ms, Memory: 14504 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -15,18 +15,18 @@ public class Main {
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 3; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+            if (i == 0) {
+                arr[i][0] = Integer.parseInt(st.nextToken());
+                arr[i][1] = Integer.parseInt(st.nextToken());
+                arr[i][2] = Integer.parseInt(st.nextToken());
+            }else{
+                arr[i][0] = Integer.parseInt(st.nextToken()) + Math.min(arr[i - 1][1], arr[i - 1][2]);
+                arr[i][1] = Integer.parseInt(st.nextToken()) + Math.min(arr[i - 1][0], arr[i - 1][2]);
+                arr[i][2] = Integer.parseInt(st.nextToken()) + Math.min(arr[i - 1][0], arr[i - 1][1]);
             }
         }
 
-        for(int i = 1; i < N; i++){
-            arr[i][0] += Math.min(arr[i - 1][1], arr[i - 1][2]);
-            arr[i][1] += Math.min(arr[i - 1][0], arr[i - 1][2]);
-            arr[i][2] += Math.min(arr[i - 1][0], arr[i - 1][1]);
-        }
-
-        int answer = Math.min(arr[N - 1][0], Math.min(arr[N- 1][1], arr[N - 1][2]));
+        int answer = Math.min(arr[N - 1][0], Math.min(arr[N - 1][1], arr[N - 1][2]));
 
         System.out.println(answer);
 
