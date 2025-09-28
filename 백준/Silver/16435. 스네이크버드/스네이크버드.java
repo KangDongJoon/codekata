4f98a149cc5323e9d46ef3c7e52c40c826e1936e
KangDongJoon <107305960+KangDongJoon@users.noreply.github.com>
2025-09-28 21:41:01 +0900

    [Silver V] Title: 스네이크버드, Time: 108 ms, Memory: 14448 KB -BaekjoonHub

---
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static int N, L;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        L = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        for (int f : arr) {
+            if (L >= f) {
+                L++;
+            } else {
+                break;
+            }
+        }
+
+        System.out.println(L);
+
+    }
+}
